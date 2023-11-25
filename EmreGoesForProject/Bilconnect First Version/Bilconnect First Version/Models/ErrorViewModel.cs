f45252b032db2eabbafbf3da56b2e555431d1d7d
EmreAkgul <emreakgulcs@gmail.com>
2023-11-25 16:39:07 +0300

    Removed Unnecessary Folder

--- EmreGoesForProject/Bilconnect First Version/Bilconnect First Version/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bilconnect_First_Version.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- EmreGoesForProject/Bilconnect First Version/Bilconnect First Version/data/Base/IEntityBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bilconnect_First_Version.data.Base
-{
-    public interface IEntityBase
-    {
-        int Id { get; set; }    
-
-    }
-}
