7d6332f89a36081170a9590613aa48860aaa872c
EmreAkgul <emreakgulcs@gmail.com>
2023-11-25 15:47:28 +0300

    Migrate to Azure Database

--- EmreGoesForProject/BilConnect/Migrations/20231125124630_servertest1.Designer.cs
+++ EmreGoesForProject/BilConnect/Migrations/20231125124630_servertest1.Designer.cs
@@ -4,6 +4,7 @@ using BilConnect.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BilConnect.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231125124630_servertest1")]
+    partial class servertest1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
