bf68fa22f6baaf56253740e6e7c7507bb46d4d90
EmreAkgul <emreakgulcs@gmail.com>
2023-12-02 16:53:23 +0300

    Added EntityBaseRepository

---
+++ BilConnect/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace BilConnect.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
+        Task<T> GetByIdAsync(int id);
+
+        Task AddAsync(T entity);
+
+        Task UpdateAsync(int id, T newEntity);
+
+        Task DeleteAsync(int id);
+    }
+    
+}
