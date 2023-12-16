d46109e461fa9661177f0ac0d2da3c8c59c65a6d
EmreAkgul <emreakgulcs@gmail.com>
2023-12-17 01:49:50 +0300

    Integrated with samed

--- BilConnect/Controllers/ClubEventsController.cs
+++ BilConnect/Controllers/ClubEventsController.cs
@@ -24,7 +24,7 @@ namespace BilConnect.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var data = await _service.GetAllAsync(n => n.ownerClub);
+            var data = await _service.GetAllAsync(null, n => n.ownerClub);
             return View(data);
         }
 
