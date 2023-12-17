2ff1501bb28e6bb45622e3b13889e59423d8aa31
EmreAkgul <emreakgulcs@gmail.com>
2023-12-17 18:26:32 +0300

    Updated Admin users view

---
+++ BilConnect/Data/ViewModels/UsersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BilConnect.Models;
+
+namespace BilConnect.Data.ViewModels
+{
+    public class UsersViewModel
+    {
+        public List<ApplicationUser> AdminUsers { get; set; }
+        public List<ApplicationUser> ClubAccountUsers { get; set; }
+        public List<ApplicationUser> RegularUsers { get; set; }
+    }
+}
