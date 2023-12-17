518f22c62587247121415aa91ac9d0809aa58411
unknown <cagri.kara@ug.bilkent.edu.tr>
2023-12-17 22:57:33 +0300

    Added comments on Controllers

--- BilConnect/Controllers/HomeController.cs
+++ BilConnect/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace BilConnect.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+       
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -26,14 +26,14 @@ namespace BilConnect.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-
+       
         public IActionResult About()
         {
             var teamMembers = new List<DeveloperInfoViewModel>
